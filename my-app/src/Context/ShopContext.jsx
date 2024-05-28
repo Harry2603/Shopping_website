8039f630d9fc11cbc3892c250e48bd621584d996
TonThatMinhVu <91868808+TonThatMinhVu@users.noreply.github.com>
2024-05-28 17:19:54 +0700

    1
    1

--- my-app/src/Context/ShopContext.jsx
+++ my-app/src/Context/ShopContext.jsx
@@ -24,13 +24,15 @@ const ShopContextProvider = (props) =>{
 
     const getTotalCartAmount = () => {
         let totalAmount = 0;
-        for(const item in cartItems){
-            if(cartItems[item]>0){
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
                 let itemInfo = all_product.find((product)=>product.id===Number(item))
                 totalAmount += itemInfo.new_price * cartItems[item];
             }
-            return totalAmount;
         }
+        return totalAmount;
     }
 
     const contextValue = {getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
